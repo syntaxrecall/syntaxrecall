d8fe494c647968638bfb447e183b24f471afbbb8
chadalen <me@chadalen.com>
2021-02-06 23:19:17 -0500

    Center content

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -7,7 +7,7 @@ interface PageProps {
 export default function Page({ children }: PageProps) {
   return (
     <div className="grid grid-cols-12">
-      <div className="col-start-5 col-span-6">
+      <div className="col-start-4 col-span-6">
         {children}
       </div>
     </div>
