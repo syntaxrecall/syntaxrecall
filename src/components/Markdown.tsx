24d4552adbb4cae164561eef8756096bcb9cbd05
chadalen <me@chadalen.com>
2021-02-07 20:32:31 -0500

    Add button to scroll to the top of the page

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import clsx from "clsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 interface MarkdownProps {
   htmlContent: string;
@@ -11,10 +13,22 @@ export default function Markdown({
   className,
 }: MarkdownProps): React.ReactElement {
   return (
-    <article
-      className={clsx("prose lg:prose-xl px-8 m-auto my-16", className)}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: htmlContent }}
-    />
+    <>
+      <article
+        className={clsx("prose lg:prose-xl m-auto px-8 my-16", className)}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: htmlContent }}
+      />
+
+      <div className="flex items-center justify-center mb-16">
+        <button
+          type="button"
+          className="flex items-center justify-center hover:bg-gray-200 cursor-pointer rounded-full w-16 h-16 focus:outline-none"
+          onClick={() => window.scrollTo(0, 0)}
+        >
+          <FontAwesomeIcon icon={faChevronUp} width="14" height="16" />
+        </button>
+      </div>
+    </>
   );
 }
