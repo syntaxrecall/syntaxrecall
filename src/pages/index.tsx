eb91fc41d08a2af819d46d8e15fc0263a6c6a711
chadalen <me@chadalen.com>
2021-02-09 23:50:16 -0500

    Fix position of searchbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,14 +11,10 @@ interface Props {
 export default function Page({ topics }: Props): React.ReactElement {
   return (
     <div className="grid grid-cols-12 min-h-screen">
-      <div className="col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 lg:col-start-4 lg:col-span-6 xl:col-start-5 xl:col-span-4 min-h-screen">
-        <div className="flex items-end" style={{ minHeight: "40%" }}>
-          <div className="w-full px-2 sm:px-0">
-            <h1 className="text-4xl text-center font-bold mb-4">
-              Syntax Recall
-            </h1>
-            <Searchbar items={topics} />
-          </div>
+      <div className="relative col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 lg:col-start-4 lg:col-span-6 xl:col-start-5 xl:col-span-4 min-h-screen">
+        <div className="absolute top-1/3 inset-x-0 px-2 sm:px-0">
+          <h1 className="text-4xl text-center font-bold mb-4">Syntax Recall</h1>
+          <Searchbar items={topics} />
         </div>
       </div>
     </div>
