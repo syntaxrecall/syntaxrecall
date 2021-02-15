44e70bfce0720732507ecacbd4932bb1fc2c7b5e
chadalen <me@chadalen.com>
2021-02-15 11:49:15 -0500

    Add meta description to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,7 @@ export default function Page({ topics }: Props): React.ReactElement {
     <>
       <Head>
         <title>Syntax Recall</title>
+        <meta name="description" content="A quick reference for developers" />
       </Head>
       <div className="grid grid-cols-12 min-h-screen">
         <div className="relative col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 lg:col-start-4 lg:col-span-6 xl:col-start-5 xl:col-span-4 min-h-screen">
