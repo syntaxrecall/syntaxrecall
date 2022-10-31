c8c0878ad36f30fe401029249521e36524910e93
chadalen <me@chadalen.com>
2022-10-31 15:07:22 -0400

    Setup nextjs v13

--- src/hooks/index.ts
+++
@@ -1,3 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export { default as useIsMounted } from "./useIsMounted";
-export { default as useDebounce } from "./useDebounce";
--- src/hooks/useIsMounted.ts
+++
@@ -1,9 +0,0 @@
-import { useRef, useEffect, MutableRefObject } from "react";
-
-export default function useIsMounted(): boolean {
-  const ref: MutableRefObject<boolean> = useRef<boolean>(true);
-  useEffect(() => {
-    ref.current = false;
-  }, []);
-  return ref.current;
-}
