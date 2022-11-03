2fef130388777f96a95d9abfea5145583fb9d20f
chadalen <me@chadalen.com>
2022-11-03 00:59:37 -0400

    chore: added search endpoint

---
+++ src/pages/api/search.tsx
@@ -0,0 +1,30 @@
+import { StatusCodes } from "http-status-codes";
+import { NextApiRequest, NextApiResponse } from "next";
+import fetch from 'isomorphic-unfetch';
+import { MeiliPostSearchResult } from "../../interfaces/MeilisearchResult";
+import { Post } from "../../interfaces/Post";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const { q } = req.query;
+
+  if (!q) {
+    res.status(StatusCodes.BAD_REQUEST).end();
+    return;
+  }
+
+  const response = await fetch('http://localhost:7700/indexes/posts/search', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ 'q': q, 'attributesToRetrieve': ['id'] })
+  });
+
+  if (response.ok) {
+    const data = await response.json() as MeiliPostSearchResult<Post>
+    res.status(StatusCodes.OK).json(data);
+    return;
+  }
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+}
