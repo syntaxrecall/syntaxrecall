0ded018a7f8467e7bf231a67a8de51c915a1eae0
chadalen <me@chadalen.com>
2022-12-21 00:06:35 -0500

    feat: set page title

--- src/pages/[subject]/[topic].tsx
+++ src/pages/[subject]/[topic].tsx
@@ -1,73 +1,90 @@
-import { getMarkdown, getTopicStaticPaths } from "../../api/api";
+import { getMarkdown, getTopicStaticPaths } from '../../api/api';
 import md from 'markdown-it';
-import clsx from "clsx";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronUp } from "@fortawesome/free-solid-svg-icons";
-import Link from "next/link";
+import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronUp } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
+import Head from 'next/head';
 
 interface Props {
   markdown: string;
+  title: string;
 }
 
-export default function Page({
-  markdown,
-}: Props) {
+export default function Page({ markdown, title }: Props) {
   return (
-    <div className="grid grid-cols-12">
-      <div className="col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 min-h-screen">
-        <div className="flex flex-col min-h-full">
-          <div className="mt-4">
-            <Link href="/">
-              <div className="rounded-full h-16 w-16 flex items-center justify-center hover:bg-gray-300 dark:text-zinc-400 dark:hover:bg-zinc-700 cursor-pointer">
-                <FontAwesomeIcon icon={faChevronLeft} width="10" height="16" />
-              </div>
-            </Link>
-          </div>
+    <>
+    <Head>
+      <title>{title}</title>
+    </Head>
+      <div className="grid grid-cols-12">
+        <div className="col-span-12 sm:col-start-2 sm:col-span-10 md:col-start-3 md:col-span-8 min-h-screen">
+          <div className="flex flex-col min-h-full">
+            <div className="mt-4">
+              <Link href="/">
+                <div className="rounded-full h-16 w-16 flex items-center justify-center hover:bg-gray-300 dark:text-zinc-400 dark:hover:bg-zinc-700 cursor-pointer">
+                  <FontAwesomeIcon
+                    icon={faChevronLeft}
+                    width="10"
+                    height="16"
+                  />
+                </div>
+              </Link>
+            </div>
 
-          <article
-            className={clsx(
-              "markdown px-2 sm:px-0 my-16 flex-grow relative",
-            )}
-            // eslint-disable-next-line react/no-danger
-            dangerouslySetInnerHTML={{ __html: markdown }}
-          />
+            <article
+              className={clsx('markdown px-2 sm:px-0 my-16 flex-grow relative')}
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{ __html: markdown }}
+            />
 
-          <div className="flex items-center justify-center mb-16">
-            <button
-              title='Go to top of page'
-              type="button"
-              className="flex items-center justify-center  hover:bg-gray-300 dark:text-zinc-400 dark:hover:bg-zinc-700 cursor-pointer rounded-full w-16 h-16 focus:outline-none"
-              onClick={() => window.scrollTo(0, 0)}
-            >
-              <FontAwesomeIcon icon={faChevronUp} width="14" height="16" />
-            </button>
+            <div className="flex items-center justify-center mb-16">
+              <button
+                title="Go to top of page"
+                type="button"
+                className="flex items-center justify-center  hover:bg-gray-300 dark:text-zinc-400 dark:hover:bg-zinc-700 cursor-pointer rounded-full w-16 h-16 focus:outline-none"
+                onClick={() => window.scrollTo(0, 0)}
+              >
+                <FontAwesomeIcon icon={faChevronUp} width="14" height="16" />
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
 interface StaticProps {
   params: {
     subject: string;
     topic: string;
-  }
+  };
 }
 
-export async function getStaticProps({ params: { subject, topic } }: StaticProps): Promise<any> {
+function getTitle(subject: string, topic: string): string {
+  const subjectFormatted = subject.substring(0, 1).toUpperCase() + subject.substring(1).toLowerCase();
+  const topicFormatted = (topic.substring(0, 1).toUpperCase() + topic.substring(1).toLowerCase()).replace('-', ' ');
+  return `${subjectFormatted} - ${topicFormatted}`;
+}
+
+export async function getStaticProps({
+  params: { subject, topic },
+}: StaticProps): Promise<any> {
   const markdown = getMarkdown(`${subject}/${topic}`, true);
   const parser = md({
     breaks: true,
     linkify: true,
   })
-  .use(require('markdown-it-emoji'))
-  .use(require('markdown-it-footnote'));
+    .use(require('markdown-it-emoji'))
+    .use(require('markdown-it-footnote'));
 
   const html = parser.render(markdown);
+  
   return {
     props: {
       markdown: html,
+      title: getTitle(subject, topic),
     },
   };
 }
@@ -78,8 +95,8 @@ export function getStaticPaths(): any {
     paths: paths.map((obj) => ({
       params: {
         subject: obj.subject,
-        topic: obj.topic, 
-      }
+        topic: obj.topic,
+      },
     })),
     fallback: false,
   };
