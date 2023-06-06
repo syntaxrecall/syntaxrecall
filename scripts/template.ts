0f2aa64322d1a9813451b62e6d742b69176e925e
chadalen <me@chadalen.com>
2023-06-06 00:33:33 -0400

    feat: created a script to generate a new data file

---
+++ scripts/template.ts
@@ -0,0 +1,27 @@
+import { input } from '@inquirer/prompts';
+import { join } from 'path';
+import * as fs from 'fs/promises';
+import * as crypto from 'crypto';
+
+async function main(): Promise<void> {
+  const fileName = await input({ message: 'Enter file name' });
+  const title = await input({ message: 'Enter title' });
+  const description = await input({ message: 'Enter a description' });
+  const dataDir = join(process.cwd(), 'data');
+
+  const headerArray = [
+    '---',
+    `id: ${crypto.randomUUID()}`,
+    `title: ${title}`,
+  ];
+
+  if (description) {
+    headerArray.push(`description: ${description}`);
+  }
+
+  headerArray.push('---');
+
+  fs.writeFile(join(dataDir, fileName.toLowerCase() + '.md'), headerArray.join('\n') + '\n');
+}
+
+main();
