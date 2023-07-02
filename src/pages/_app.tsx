36ebe61ab7bb98f4bd5a8494ccbb02555552e603
chadalen <me@chadalen.com>
2023-07-01 23:26:12 -0400

    fix: background color

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,5 @@ import '../styles/global.css';
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <div className='dark:bg-neutral-900 bg-gray-200'><Component {...pageProps} /></div>
+  return <div><Component {...pageProps} /></div>
 }
